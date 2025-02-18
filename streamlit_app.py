352de61fd629c1f65d048a10008b44b53d74bf03
rickykaky <romaric.kaki@groupe-esigelec.org>
2025-02-18 12:16:48 +0100

    Update V5

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,17 @@ def get_model():
     return ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 model = get_model()
  
- 
+# Fonction pour ajouter un message à l'historique
+def add_message(role, content):
+    st.session_state.messages.append({"role": role, "content": content})
+
  
 st.title("Dark-GPT")
  
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Entrez une situation pour avoir un meme"}]
-"""
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-        
 
- # Fonction pour ajouter un message à l'historique
-def add_message(role, content):
-    st.session_state.messages.append({"role": role, "content": content})
-"""
 # Afficher l'historique des messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -51,5 +44,6 @@ if prompt := st.chat_input("Générez votre meme"):
     with st.chat_message("assistant"):
         st.markdown(response)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    add_message(assistant, response)
+    #st.session_state.messages.append({"role": "assistant", "content": response})
     st.write (result.id)
